cc1ae29da577cb818b04fbccd70a1baf639bae8a
Jonathan Engdahl <engdahl@windstream.net>
2022-03-08 16:47:31 -0500

    add "yield" to test.cpp

--- test.cpp
+++ test.cpp
@@ -2,10 +2,13 @@
 
 #include "exports.hpp"
 #include "thread.hpp"
+#include "threadFIFO.hpp"
 
 Thread x;
 
 char stack[128] __attribute__((aligned(8)));
+
+threadFIFO<16> DeferFIFO;
 
 void thread();
 
@@ -19,6 +22,9 @@ int start(int argc, char *const argv[])
     Thread::spawn(thread, stack);
     printf("main: thread done = %u\n", Thread::done(stack));
     x.resume();
+    printf("main: returned from resume, trying undefer next\n");
+    undefer();
+    undefer();
     printf("main: thread done = %u\n", Thread::done(stack));
     printf("main: test complete\n");
 
@@ -30,7 +36,9 @@ void thread()
     {
     printf("thread: started\n");
     x.suspend();
-    printf("thread: resumed, exiting\n");
+    printf("thread: resumed\n");
+    yield();
+    printf("thread: returned from yield, exiting\n");
     }
 
 

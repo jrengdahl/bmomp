e8250a8b4713aac6075a121ce7d434ce1a6de565
Jonathan Engdahl <engdahl@windstream.net>
2022-03-16 14:55:06 -0400

    remove large alignment of thread stacks
    comments

--- thread.cpp
+++ thread.cpp
@@ -1,51 +1,81 @@
-#include <stdint.h>
+// Thread.cpp
+// Implementation of the Thread class.
 
+#include <stdint.h>
 #include "thread.hpp"
 
+// Define the maximum depth of teh thread stack -- how deep can "resume" calls be nested.
+// I believe 32 is a ridiculously large nesting depth.
 const unsigned THREAD_DEPTH = 32;
 
+// This is the pending thread stack.
 static Thread thread_stack[THREAD_DEPTH];
 
-
+// Push the current thread onto the pending stack, and
+// resume a thread saved in a Thread object.
 void Thread::resume()
     {
     __asm__ __volatile__(
-"   mov ip, sp                                      \n"
-"   stmdb r11!, {r4, r5, r6, r7, r8, r10, ip, lr}   \n"
-"   ldmia r0,   {r4, r5, r6, r7, r8, r10, ip, lr}   \n"
-"   mov sp, ip                                      \n"
-    );
+"   mov ip, sp                                      \n"         // sp cannot be pushed, so move it to ip first
+"   stmdb r11!, {r4, r5, r6, r7, r8, r10, ip, lr}   \n"         // push all the non-volatile registers of the current thread onto the thread stack
+"   ldmia r0,   {r4, r5, r6, r7, r8, r10, ip, lr}   \n"         // load all the non-volatile registers of the new thread into the registers
+"   mov sp, ip                                      \n"         // move ip back to sp
+    );                                                          // return to the new thread
     }
 
+// Suspend the current thread into a Thread object,
+// pop the next thread from the pending stack into the
+// register set, and resume running it.
 void Thread::suspend()
     {
     __asm__ __volatile__(
-"   mov ip, sp                                      \n"
-"   stmia r0,   {r4, r5, r6, r7, r8, r10, ip, lr}   \n"
-"   ldmia r11!, {r4, r5, r6, r7, r8, r10, ip, lr}   \n"
-"   mov sp, ip                                      \n"
-    );
+"   mov ip, sp                                      \n"         // save sp in ip so it can be saved using STM
+"   stmia r0,   {r4, r5, r6, r7, r8, r10, ip, lr}   \n"         // save the non-volatile registers of the current thread into the Thread instance
+"   ldmia r11!, {r4, r5, r6, r7, r8, r10, ip, lr}   \n"         // pop the non-volatile registers of the most recently active thread on the pending stack
+"   mov sp, ip                                      \n"         // restore sp
+    );                                                          // return to the previously pending thread
     }
 
+
+// Start a new thread, given it's code and initial stack pointer.
+// args: fn:    a pointer to a subroutine which will be run as a thread
+//       newsp: the initial stack pointer of the new thread
+
+// The newsp must point to the second word from the high end of the stack.
+// The word pointed to by newsp is reserved to pass the thread's return value
+// during the rendezvous at the completion of the thread. The second word
+// past newsp is used for the thread's "done" flag.
+//
+// The thread which calls spawn is pushed to the pending stack as if it
+// had called "resume". The calling thread will be resumed the first time
+// the new thread suspends itself. The calling thread will continue
+// executing after the call to "start". It will most likely continue executing
+// before the new thread terminates.
+//
+// When the new thread terminates by returning from it's subroutine, it returns
+// to this code, which will run the next thread from the pending stack. This
+// is not necessarily the thread which created the terminated thread.
 
 void Thread::start(THREADFN *fn, char *newsp)
     {
     __asm__ __volatile__(
-"   mov ip, sp                                      \n"
-"   stmdb r11!, {r4, r5, r6, r7, r8, r10, ip, lr}   \n"
+"   mov ip, sp                                      \n"             // save the sp of the calling thread in a register that can be pushed
+"   stmdb r11!, {r4, r5, r6, r7, r8, r10, ip, lr}   \n"             // push the calling thread onto the pending thread stack
 
-"   mov sp, %[newsp]                                \n"
-"   mov r1, #0                                      \n"
-"   str r1, [sp, #0]                                \n"
-"   str r1, [sp, #4]                                \n"
+"   mov sp, %[newsp]                                \n"             // setup the new thread's stack
+"   mov r1, #0                                      \n"             //
+"   str r1, [sp, #0]                                \n"             // clear the return value
+"   str r1, [sp, #4]                                \n"             // and the "done" flag
 
-"   blx %[fn]                                       \n"
+"   blx %[fn]                                       \n"             // start executing the code of the new thread
 
-"   mov r1, #1                                      \n"
-"   str r0, [sp, #0]                                \n"
-"   str r1, [sp, #4]                                \n"
+// when the new thread terminates, it returns here...
 
-"   ldmia r11!, {r4, r5, r6, r7, r8, r10, ip, lr}   \n"
+"   mov r1, #1                                      \n"             //
+"   str r0, [sp, #0]                                \n"             // save the return value
+"   str r1, [sp, #4]                                \n"             // set the "done" flag
+
+"   ldmia r11!, {r4, r5, r6, r7, r8, r10, ip, lr}   \n"             // pop the next thread from the pending thread stack, and return to it
 "   mov sp, ip                                      \n"
 
     :
@@ -55,10 +85,12 @@ void Thread::start(THREADFN *fn, char *newsp)
     }
 
 
+// init the threading system
+// At present, this consists only of setting the pending thread stack pointer
 void Thread::init()
     {
     __asm__ __volatile__(
-"   mov r11, %[tsp]"
+"   mov r11, %[tsp]"                                                // init the thread stack pointer
     :
     : [tsp]"r"(&thread_stack[THREAD_DEPTH])
     :
--- thread.hpp
+++ thread.hpp
@@ -1,10 +1,62 @@
+// thread.hpp
+
+// A simple but very fast threading system for bare metal ARM processors.
+//
+// A thread is represented by its non-volatile registers.
+// A thread can be in one of three states;
+// -- running: it currently is running on the CPU and its state is contained in the CPU registers.
+// -- suspended: the thread is saved in a Thread object, and is waiting to be resumed. A thread in this
+//    state is typically waiting for an event.
+// -- pending: a thread that has resumed another thread has it's state pushed on the pending thread stack.
+//    A thread in this state is waiting for a running thread to suspend itself.
+//
+// The threading system is non-preemptive. That is, a thread never leaves the running state other than
+// by its own action (suspend itself, resume another thread, or terminate).
+//
+// A thread consists of:
+// -- a subroutine. Multiple threads can be running the same code.
+// -- a context. The context consists of the non-volatile registers (r4-r11, sp, lr, on some systems
+//    excluding fixed registers such as r9). The volatile registers r0-r3 and ip are no members of a
+//    saved context because this threading system is non-preemptive -- a thread state change is only
+//    initiated by a subroutine call, and volatile registers do not need to be saved across a call.
+//    Furthermore, the pc is saved in lr by a subroutine call, thus the pc is not explicitly saved.
+// -- a stack, pointer to by sp. The Thread functions that receive a stack as an argument are passed a
+//    reference to an array of char. In this way the templated functions can determine the stack size,
+//    whereas if a pointer were passed the size information would be lost.
+//
+// Multiple threads can be running virtually concurrently on a single CPU. In actuality, virtually
+// concurrent threads run serially. Multiple threads can actually run concurrently on multiple cores.
+//
+// Memory available to a thread is either local (stack) or globally shared with all other threads.
+// This implmentation does not provide thread-private static memory. On single and multi-core systems
+// the threads share the same address space. It is assumed that the address space is coherent (all
+// threads see the same content for all addresses, regardless of cacheing or which core they are on).
+//
+// This implementation stores the thread stack pointer (points to a stack of pending threads, not to be
+// confused with the notmal sp) in r11. This removes one more register from the pool allocatable by the
+// compiler, but has the following significant benefit:
+// -- the thread switching code becomes much faster and simpler.
+// -- the compiler requires fewer registers, thus the threadFIFO algorithm can be implemented using
+//    only the five volatile registers. This greatly simplifies the threadFIFO suspend and resume routines.
+// 
+// Excluding the subroutine call and return, a thread switch costs only four instructions. On a 500 MHz
+// CPU this would be 40 nanoseconds. Compare this to 10 microseonds for a thread switch on a preemptive
+// RTOS. Obviously, a fully preemptive, prioritized, time-sliced RTOS has its advantages, but where sheer
+// performance outweighs these advantages, a non-preemtive system such as this is necessary. The other
+// alternative would be to implement the firmware as a complex state machine, but 45 years experience coding
+// real-time firmware has convinced me that a set of cooperating threads are much easier to design, implement,
+// and debug than a huge single-threaded state machine.
+
+
 #ifndef THREAD_H
 #define THREAD_H
 
 #include <stdint.h>
 
+// the code of a thread
 typedef void THREADFN();
 
+// the context of a thread
 class Thread
     {
     private:
@@ -21,23 +73,28 @@ class Thread
     // r9 is reserved by u-boot for the gd pointer
     // r11 is reserved by Thread for the thread stack pointer
 
-    static void start(THREADFN *fn, char *sp);
+    static void start(THREADFN *fn, char *sp);                  // an internal function to start a new thread
 
     public:
 
-    void resume();
-    void suspend();
+    void resume();                                              // resume a suspended thread
+    void suspend();                                             // suspend self until resumed
 
+    // spawn a new thread
     template<unsigned N>
     static void spawn(THREADFN *fn,         // code
-                      char (&stack)[N])     // reference to the stack)
+                      char (&stack)[N])     // reference to the stack. The template can determine the stack size from the reference.
         {
-        start(fn, &stack[N-8]);
+        start(fn, &stack[N-8]);             // reserve two words at stack top, then call the start function, passing the initial sp
         }
 
 
-    static void init();
+    static void init();                     // powerup init of the thread system (inits the thread stack pointer in r11)
 
+    // Thread::done -- test whether a thread is running
+    // arg: the thread's stack
+    // return: true if the thread is done, false if it is still running.
+    // The result is only valid after the thread has been started (and you are waiting for it to complete).
     template<unsigned N>
     __attribute__((__noinline__))
     static bool done(char (&stack)[N])
